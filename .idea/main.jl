e65241e5d3eaa5e9eb6a03454e0795c2e0599b94
ElComadore <coen.olofsson@gmail.com>
2022-10-17 16:41:15 +0200

    A first, proper commit.

---
+++ .idea/main.jl
@@ -0,0 +1,9 @@
+#=
+main:
+- Julia version: 
+- Author: coeno
+- Date: 2022-10-15
+=#
+import
+
+(1 + 2im)/(2 - 3im)
---
+++ main.jl
@@ -0,0 +1,6 @@
+#=
+main:
+- Julia version: 1.8.2
+- Author: coeno
+- Date: 2022-10-17
+=#
